7907cd4db6bec70622e14628d55bca2defca560e
Pavelweb22 <pavelweb2016@yandex.ru>
2022-04-08 22:57:15 +0300

    hello world

--- T-Converter/ViewController.swift
+++ T-Converter/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.minimumValue = 0
+            slider.maximumValue = 100
+            slider.value = 0
+        }
     }
-
-
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let temperatureCelsius = Int(round(slider.value))
+        celsiusLabel.text = "\(temperatureCelsius)ºC"
+        
+        let temperatureFahrenheit = (temperatureCelsius * 9 / 5) + 32
+        fahrenheitLabel.text = "\(temperatureFahrenheit)ºF"
+    }
 }
 
+
+
+
+//hello world
+
+//@IBOutlet weak var celsiusLabel: UILabel!
+//@IBOutlet weak var fahrenheitLabel: UILabel!
+//@IBOutlet weak var slider: UISlider! {
+//    didSet {
+//        slider.minimumValue = 0
+//        slider.maximumValue = 100
+//        slider.value = 0
+//    }
+//}
+//
+//
+//@IBAction func sliderSet(_ sender: UISlider) {
+//    let temperatureCelsius = Int(round(slider.value))
+//    celsiusLabel.text = "\(temperatureCelsius)ºC"
+//
+//    let temperatureFahrenheit = Int(round(slider.value * 9/5) + 32)
+//    fahrenheitLabel.text = "\(temperatureFahrenheit)ºF"
+//}
